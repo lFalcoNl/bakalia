54ff4e88583b42a219ed4b8bd79b2176eb400d90
Makohon Pavlo <makohon@Makohons-Mac-mini.local>
2025-05-25 15:17:42 +0300

    vercel integration

--- api/server.js
+++ api/server.js
@@ -49,8 +49,8 @@ app.use((err, req, res, next) => {
   res.status(500).json({ msg: 'Внутрішня помилка сервера' })
 })
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+// const PORT = process.env.PORT || 5000
+// app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
 
 module.exports = app
 module.exports.handler = serverless(app);
