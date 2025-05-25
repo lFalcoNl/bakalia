47bc6722bbea573b4a0f754924288761c702359a
Makohon Pavlo <makohon@Makohons-Mac-mini.local>
2025-05-25 15:52:59 +0300

    vercel integration api server

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,15 +1,13 @@
-// api/index.js
+// api/server.js
 require('dotenv').config()
-const serverless = require('serverless-http')
 const express = require('express')
 const cors = require('cors')
 const connectDB = require('./config/db')
 
-
 const app = express()
 
 const allowed = [
-  process.env.FRONTEND_URL,           
+  process.env.FRONTEND_URL,
   'http://localhost:5173'
 ]
 
@@ -17,9 +15,7 @@ app.use(cors({
   origin(origin, callback) {
     // allow requests with no origin (e.g. mobile apps, curl)
     if (!origin) return callback(null, true)
-    if (allowed.includes(origin)) {
-      return callback(null, true)
-    }
+    if (allowed.includes(origin)) return callback(null, true)
     callback(new Error(`Origin ${origin} not allowed`))
   },
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
@@ -49,8 +45,8 @@ app.use((err, req, res, next) => {
   res.status(500).json({ msg: 'Внутрішня помилка сервера' })
 })
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+// —––– Коментуємо, бо на Vercel Serverless ця частина не потрібна
+// const PORT = process.env.PORT || 5000
+// app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
 
 module.exports = app
-module.exports.handler = serverless(app);
