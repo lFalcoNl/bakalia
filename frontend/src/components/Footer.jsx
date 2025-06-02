9cb9ab7ca66b6567b68a0226835bd52c0d00d7f7
lFalcoNl <64079334+lFalcoNl@users.noreply.github.com>
2025-06-02 17:48:05 +0300

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -9,7 +9,8 @@ export default function Footer() {
           <img src="/images/logo/mainLogo.png" alt="Logo" className="fade-in fade-in h-20 m-3 rounded-md" />
         </Link>
         <div className="fade-in fade-in text-center md:text-left space-y-1">
-          <p>Адреса: вул. Бічна Промислова 23А, м.Шептицький</p>
+          <p>Адреса: м.Шептицький</p>
+{/*           <p>Адреса: вул. Бічна Промислова 23А, м.Шептицький</p> */}
           <p>Телефон: +380986150065</p>
           <p>Email: bakaliyniydvir@gmail.com</p>
         </div>
