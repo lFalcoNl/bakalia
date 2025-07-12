9d4d63d319d1609ef32453da121b6c4d1cedf091
Makohon Pavlo <makohon@Makohons-Mac-mini.local>
2025-07-12 15:50:49 +0300

    scroll fix

--- frontend/src/components/ScrollToTop.jsx
+++ frontend/src/components/ScrollToTop.jsx
@@ -2,11 +2,13 @@ import { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 
 export default function ScrollToTop() {
-    const { pathname } = useLocation()
+  const { pathname } = useLocation()
 
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, [pathname])
+  useEffect(() => {
+    // Виправлення для деяких випадків, коли window.scrollTo не працює
+    document.documentElement.scrollTop = 0
+    document.body.scrollTop = 0
+  }, [pathname])
 
-    return null
+  return null
 }
